4d02327c7ba50ccd49a8dc393948ffbdcd139f92
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2026-03-11 14:37:44 +0100

    chore: bump version to 0.17.1.post5

--- agent_browser/version.py
+++ agent_browser/version.py
@@ -1,5 +1,5 @@
 """Version information for agent-browser Python wrapper."""
 
-__version__ = "0.17.1.post4"
+__version__ = "0.17.1.post5"
 __agent_browser_version__ = "0.17.1"
 __node_version__ = "22.14.0"
